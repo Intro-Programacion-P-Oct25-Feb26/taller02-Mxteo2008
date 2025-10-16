d734905274e074f1add4cc56f95cf3bc78af3f92
Mxteo2008 <mateo.jimenez.bvm@gmail.com>
2025-10-16 11:20:09 -0500

    primer commit

---
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author UTPL
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Ejemplo básico de Java Versionado a través de Git Manejado desde GitHub");
+    }
+}
